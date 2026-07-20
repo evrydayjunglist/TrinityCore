f46bd0accb7ece03ab6d4086e704a7ce7bd5f1de
evrydayjunglist <whatsatangelo@gmail.com>
2026-07-20 00:16:29 -0700

    feat(skyriding): send facing-relative Skyward Ascent impulse

---
+++ sql/updates/world/master/2026_07_19_00_world_evry_skyriding_skyward_ascent.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` = 372610 AND `ScriptName` = 'spell_skyriding_skyward_ascent';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(372610, 'spell_skyriding_skyward_ascent');
